13dd6aa46a749bf814382664965a9e2f258cc59d
Camilo Sabogal <lobogral@gmail.com>
2022-01-06 19:38:13 -0500

    Agrego contador para simplificar instancia de arreglos

--- src/Launcher.java
+++ src/Launcher.java
@@ -35,19 +35,22 @@ public class Launcher {
         ventana.addKeyListener(new Listener(jugador2, KeyEvent.VK_I, KeyEvent.VK_K));
 
         Lienzo[] lienzos = {complemento, escenario, pelota, jugador1, jugador2};
-
-        Interaccion[] interacciones = new Interaccion[6];
-        interacciones[0] = new InteJugadorEscenario(jugador1, escenario);
-        interacciones[1] = new InteJugadorEscenario(jugador2, escenario);
-        interacciones[2] = new IntePuntaje(pelota, escenario, jugador1, jugador2);
-        interacciones[3] = new IntePelotaJugador(pelota, jugador1);
-        interacciones[4] = new IntePelotaJugador(pelota, jugador2);
-        interacciones[5] = new IntePelotaEscenario(pelota, escenario);
+        int cont;
         
+        cont = 0;
+        Interaccion[] interacciones = new Interaccion[6];
+        interacciones[cont++] = new InteJugadorEscenario(jugador1, escenario);
+        interacciones[cont++] = new InteJugadorEscenario(jugador2, escenario);
+        interacciones[cont++] = new IntePuntaje(pelota, escenario, jugador1, jugador2);
+        interacciones[cont++] = new IntePelotaJugador(pelota, jugador1);
+        interacciones[cont++] = new IntePelotaJugador(pelota, jugador2);
+        interacciones[cont++] = new IntePelotaEscenario(pelota, escenario);
+        
+        cont = 0;
         Operacion[] operaciones = new Operacion[3];
-        operaciones[0] = new Dibujar(anchoJuego, altoJuego, ventana.getLapiz(), lienzos);
-        operaciones[1] = new Interactuar(interacciones);
-        operaciones[2] = new Dormir();
+        operaciones[cont++] = new Dibujar(anchoJuego, altoJuego, ventana.getLapiz(), lienzos);
+        operaciones[cont++] = new Interactuar(interacciones);
+        operaciones[cont++] = new Dormir();
         
         Pong pong = new Pong(operaciones);
         pong.iniciarJuego();
